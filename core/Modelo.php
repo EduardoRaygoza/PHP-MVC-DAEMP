9144a4fa0150fd4ba544e455483d16b9e89ec4e1
Eduardo Raygoza <eduardoraygoza@gmail.com>
2020-12-12 16:36:17 -0600

    Primer commit

---
+++ core/Modelo.php
@@ -0,0 +1,33 @@
+<?php
+
+class Modelo extends Entidad{
+    private $table;
+
+    public function __construct($table){
+        $this->table = (string) $table;
+        parent::__construct($table);
+    }
+
+    public function ejecutarSQL($query){
+        $query = $this->getDB()->query($query);
+        if($query){
+            if($query->num_rows > 1) {
+                while($row = $query->fetch_object()){
+                    $resultSet[] = $row;
+                }
+            } elseif($query->num_rows == 1) {
+                if($row = $query->fetch_object()){
+                    $resultSet = $row;
+                }
+            } else {
+                $resultSet = true;
+            }
+        } else {
+            $resultSet = false;
+        }
+
+        return $resultSet;
+    }
+}
+
+?>
